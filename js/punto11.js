39182b87c69dd4b2f351e54eaff73d6b12031ce4
maxivega0 <maximilianovega600@gmail.com>
2023-03-07 17:39:43 -0300

    resolucion punto 11

--- js/punto11.js
+++ js/punto11.js
@@ -1,1 +1,25 @@
 //! 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+const num = parseInt(prompt("Escriba un numero"));
+
+switch (num) {
+    case num:
+        if(num % 2 === 0){
+            document.write(`El numero ${num} es divisible en 2.<br>`);
+        }
+    case num:
+        if(num % 3 === 0){
+            document.write(`El numero ${num} es divisible en 3.<br>`);
+        }
+    case num:
+        if(num % 5 === 0){
+            document.write(`El numero ${num} es divisible en 5.<br>`);
+        }
+    case num:
+        if(num % 7 === 0){
+            document.write(`El numero ${num} es divisible en 7.<br>`);
+        }
+        break;
+    default:
+        document.write(`No es divisible por ningun caso`)
+        break;
+}
